c9c0ca20ded6af81b9ea1946068e6ef368304fd0
David Sabeti and Rob Dimsdale <dsabeti@pivotal.io>
2014-10-29 16:43:53 -0700

    Optionally write a pidfile if specified by -pidfile flag

--- registrar_app.go
+++ registrar_app.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"flag"
+	"io/ioutil"
 	"os"
+	"strconv"
 
 	"github.com/cloudfoundry-incubator/route-registrar/config"
 	. "github.com/cloudfoundry-incubator/route-registrar/healthchecker"
 	. "github.com/cloudfoundry-incubator/route-registrar/registrar"
 )
 
+var (
+	pidfile = flag.String("pidfile", "", "Location of file to write to.")
+)
+
 func main() {
+	flag.Parse()
+	if *pidfile != "" {
+		err := ioutil.WriteFile(*pidfile, []byte(strconv.Itoa(os.Getpid())), 0644)
+		if err != nil {
+			panic(err)
+		}
+	}
+
 	config := config.InitConfigFromFile("registrar_settings.yml")
 	registrar := NewRegistrar(config)
 	//add health check handler
