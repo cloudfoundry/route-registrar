96bc622f89bb0366723086d5a5bf89e3ddfe5a39
Amelia Downs <amelia.downs@broadcom.com>
2025-01-17 18:36:52 +0000

    Revert "Revert "refactor: rename lb_algo to loadbalancing""
    This reverts commit e4d2ffaf2a12d3110b98476c83a1e44c64cd4683.

--- config/config.go
+++ config/config.go
@@ -69,7 +69,7 @@ type RouteSchema struct {
 }
 
 type Options struct {
-	LoadBalancingAlgorithm LoadBalancingAlgorithm `json:"lb_algo,omitempty" yaml:"lb_algo,omitempty"`
+	LoadBalancingAlgorithm LoadBalancingAlgorithm `json:"loadbalancing,omitempty" yaml:"loadbalancing,omitempty"`
 }
 
 type LoadBalancingAlgorithm string
